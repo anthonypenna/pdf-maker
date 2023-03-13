81d27cf0e210f9ae4ab980dd42f9c9d6984ebe40
Anthony Penna <penna1296@gmail.com>
2023-03-13 19:39:27 +0100

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-import { main } from './main'
+import html2canvas from 'html2canvas'
+import jsPDF from 'jspdf'
+import 'virtual:windi.css'
 
-main()
+async function takePageScreenshot(page: HTMLElement) {
+	const canvas = await html2canvas(page)
+	return canvas.toDataURL('image/webp')
+}
+
+function convertImageToPDF(image: string) {
+	const pdf = new jsPDF('portrait', 'mm', 'a4', false)
+	pdf.addImage(image, 'WEBP', 0, 0, 210, 297)
+	return pdf
+}
+
+const downloadButton = document.querySelector('#download_button') as HTMLButtonElement
+
+downloadButton.addEventListener('click', async () => {
+	const page = document.querySelector('#page') as HTMLElement
+	const pageScreenshot = await takePageScreenshot(page)
+	const pdf = convertImageToPDF(pageScreenshot)
+	pdf.save('example.pdf')
+})
